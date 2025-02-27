ac778ece2a84051022e2072d99e9f3cf8eb3e51d
abyanfalah <abyanf410@gmail.com>
2025-02-27 10:11:10 +0700

    PATCH Auth helper

--- src/main/java/id/asqi/idesa/bumdes/core/auth/Auth.java
+++ src/main/java/id/asqi/idesa/bumdes/core/auth/Auth.java
@@ -1,4 +1,18 @@
 package id.asqi.idesa.bumdes.core.auth;
 
+import id.asqi.idesa.bumdes.model.UserBumdes;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class Auth {
+	public static UserBumdes getUserBumdes() {
+		return (UserBumdes) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	}
+
+	public static Long id () {
+		return getUserBumdes().getId();
+	}
+	public static String username () {
+		return getUserBumdes().getUsername();
+	}
+
 }
