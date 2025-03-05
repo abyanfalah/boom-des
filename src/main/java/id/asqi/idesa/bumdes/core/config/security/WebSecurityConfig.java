dce80b924bcc3b7b713b75fac541766f7de68102
abyanfalah <abyanf410@gmail.com>
2025-03-05 10:38:46 +0700

    allow UserBumdes generation endpoint

--- src/main/java/id/asqi/idesa/bumdes/core/config/security/WebSecurityConfig.java
+++ src/main/java/id/asqi/idesa/bumdes/core/config/security/WebSecurityConfig.java
@@ -69,6 +69,7 @@ public class WebSecurityConfig {
 						/*test*/
 //						.requestMatchers("/test/**").permitAll()
 						.requestMatchers("/test").permitAll()
+						.requestMatchers("/test/generate-user-bumdes").permitAll()
 						.requestMatchers("/test/principal").authenticated()
 						.requestMatchers("/test/protected").permitAll()
 
