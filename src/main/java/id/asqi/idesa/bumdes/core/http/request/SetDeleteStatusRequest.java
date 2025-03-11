c37d5fc452fdaa58b8c4bff598f3a2447cdff606
abyanfalah <abyanf410@gmail.com>
2025-03-11 11:08:16 +0700

    PATCH SetDeleteStatusRequest

--- src/main/java/id/asqi/idesa/bumdes/core/http/request/SetDeleteStatusRequest.java
+++ src/main/java/id/asqi/idesa/bumdes/core/http/request/SetDeleteStatusRequest.java
@@ -1,12 +1,14 @@
 package id.asqi.idesa.bumdes.core.http.request;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SetDeleteStatusRequest extends IdNumberRequest{
-	@NotNull
 	private Boolean isDeleted = true;
+
+	public Boolean getIsDeleted(){
+		return isDeleted == null || isDeleted;
+	}
 }
