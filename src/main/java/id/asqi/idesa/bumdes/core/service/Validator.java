2419c9a6e2a54569842f73484b397204267195b5
abyanfalah <abyanf410@gmail.com>
2025-03-11 11:06:43 +0700

    rename validator

--- src/main/java/id/asqi/idesa/bumdes/core/service/Validator.java
+++ src/main/java/id/asqi/idesa/bumdes/core/service/Validator.java
@@ -1,15 +1,14 @@
 package id.asqi.idesa.bumdes.core.service;
 
-import id.asqi.idesa.bumdes.core.Constants;
 import id.asqi.idesa.bumdes.core.component.exception.InvalidOperationException;
 
 import java.time.LocalDateTime;
 
-public class Validation {
+public class Validator {
 	public static <T> void deletionCheck(boolean isDeleted, Class<T> clazz) {
 		if(!isDeleted) return;
 
-		String className = Constants.pascalToSentenceCase(clazz.getSimpleName());
+		String className = pascalToSentenceCase(clazz.getSimpleName());
 
 		throw new InvalidOperationException(className.concat(" ini sudah dihapus"));
 	}
@@ -17,8 +16,14 @@ public class Validation {
 	public static <T> void deletionCheck(LocalDateTime deletedAt, Class<T> clazz) {
 		if(deletedAt == null) return;
 
-		String className = Constants.pascalToSentenceCase(clazz.getSimpleName());
+		String className = pascalToSentenceCase(clazz.getSimpleName());
 
 		throw new InvalidOperationException(className.concat(" ini sudah dihapus"));
+	}
+
+	private static String pascalToSentenceCase (String pascalCaseString) {
+		String result = pascalCaseString.replaceAll("([a-z])([A-Z])", "$1 $2");
+		result = result.substring(0, 1).toUpperCase() + result.substring(1);
+		return result;
 	}
 }
